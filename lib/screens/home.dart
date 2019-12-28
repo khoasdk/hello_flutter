aa4c7bdd6b5455608c64fe1bc3ca5bc2708659ee
Khoa Vo <khoa.sdk@gmail.com>
2019-12-28 10:57:35 +0700

    Add greeting by time for home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,19 +4,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Text in App Bar"),
-        ),
-        body: Material(
-          color: Colors.deepPurple,
-          child: Center(
-            child: Text(
-              "Hello Flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 36),
-            ),
+      appBar: AppBar(
+        title: Text("Text in App Bar"),
+      ),
+      body: Material(
+        color: Colors.deepPurple,
+        child: Center(
+          child: Text(
+            sayHello(),
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 36),
           ),
         ),
-      );
+      ),
+    );
+  }
+
+  String sayHello() {
+    String hello;
+    DateTime now = DateTime.now();
+    int hour = now.hour;
+    int minute = now.minute;
+
+    if (hour < 12) {
+      hello = "Good Morning";
+    } else if (hour < 18) {
+      hello = "Good Afternoon";
+    } else {
+      hello = "Good Evening";
+    }
+
+    String time = (minute < 10) ? "0" + minute.toString() : minute.toString();
+
+    return "It's now " + hour.toString() + ":" + time.toString() + ".\n" + hello;
   }
 }
